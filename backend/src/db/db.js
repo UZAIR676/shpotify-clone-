6ef6e23a9cb2d0230a69a2b75e30b56f7a1573a3
RANA MUAHAMMAD UZAIR <141179128+UZAIR676@users.noreply.github.com>
2024-05-25 21:31:56 +0500

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://uzairlegend480:bYnkcIJgZvkTZGzp@cluster0.teaisqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('../././')
 .then (()=>{
     console.log("connected to database ");
 })
